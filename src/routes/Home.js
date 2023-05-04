37ca6961926ab4bf36a0321e1206ae0d0abfeb3a
Duong-D-K <tuthien1508.2k@gmail.com>
2023-05-04 10:55:33 +0800

    First Version

---
+++ src/routes/Home.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+
+class Home extends Component {
+    render() {
+        const { isLoggedIn } = this.props;
+        let linkToRedirect = isLoggedIn ? "/system/user-manage" : "/login";
+        //check login, if logged in then redirect to route "/system/user-manager"
+        return <Redirect to={linkToRedirect} />;
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        isLoggedIn: state.admin.isLoggedIn,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
