3929a6e1790649232f12a832bdaefeedf0cb37a4
Duong-D-K <tuthien1508.2k@gmail.com>
2023-05-26 23:20:47 +0800

    finish 50 lesson: Complete Clone BookingCare Interface

--- src/containers/HomePage/Section/HandBook.js
+++ src/containers/HomePage/Section/HandBook.js
@@ -1,42 +1,41 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import "./Specialty.scss";
 import { FormattedMessage } from "react-intl";
 import Slider from "react-slick";
 
-class Specialty extends Component {
+class Handbook extends Component {
     render() {
         return (
-            <div className="section-share section-specialty">
+            <div className="section-share section-handbook">
                 <div className="section-container">
                     <div className="section-header">
-                        <span className="title-section">Chuyên Khoa Phổ Biến</span>
-                        <button className="btn-section">Xem Thêm</button>
+                        <span className="title-section">Cẩm Nang</span>
+                        <button className="btn-section">Tất Cả Bài Viết</button>
                     </div>
                     <div className="section-body">
                         <Slider {...this.props.settings}>
                             <div className="section-customize">
-                                <div className="bg-image section-specialty "></div>
+                                <div className="bg-image section-handbook "></div>
                                 <div className="section-customize-text">Cơ Xương Khớp 1</div>
                             </div>
                             <div className="section-customize">
-                                <div className="bg-image section-specialty "></div>
+                                <div className="bg-image section-handbook "></div>
                                 <div className="section-customize-text">Cơ Xương Khớp 2</div>
                             </div>
                             <div className="section-customize">
-                                <div className="bg-image section-specialty "></div>
+                                <div className="bg-image section-handbook "></div>
                                 <div className="section-customize-text">Cơ Xương Khớp 3</div>
                             </div>
                             <div className="section-customize">
-                                <div className="bg-image section-specialty "></div>
+                                <div className="bg-image section-handbook "></div>
                                 <div className="section-customize-text">Cơ Xương Khớp 4</div>
                             </div>
                             <div className="section-customize">
-                                <div className="bg-image section-specialty "></div>
+                                <div className="bg-image section-handbook "></div>
                                 <div className="section-customize-text">Cơ Xương Khớp 5</div>
                             </div>
                             <div className="section-customize">
-                                <div className="bg-image section-specialty "></div>
+                                <div className="bg-image section-handbook "></div>
                                 <div className="section-customize-text">Cơ Xương Khớp 6</div>
                             </div>
                         </Slider>
@@ -61,4 +60,4 @@ const mapDispatchToProps = (dispatch) => {
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Specialty);
+export default connect(mapStateToProps, mapDispatchToProps)(Handbook);
---
+++ src/containers/HomePage/Section/OutStandingDoctor.js
@@ -0,0 +1,102 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { FormattedMessage } from "react-intl";
+import Slider from "react-slick";
+
+class OutStandingDoctor extends Component {
+    render() {
+        return (
+            <div className="section-share section-outstanding-doctor">
+                <div className="section-container">
+                    <div className="section-header">
+                        <span className="title-section">Bác Sĩ Nổi Bật Tuần Qua</span>
+                        <button className="btn-section">Xem Thêm</button>
+                    </div>
+                    <div className="section-body">
+                        <Slider {...this.props.settings}>
+                            <div className="section-customize">
+                                <div className="customize-border">
+                                    <div className="outer-bg">
+                                        <div className="bg-image section-outstanding-doctor"></div>
+                                    </div>
+                                    <div className="postion text-center">
+                                        <div className="section-customize-text">Phó Giáo sư, Tiến sĩ, Bác sĩ cao cấp Nguyễn Duy Hưng 1</div>
+                                        <div className="">Cơ Xương Khớp</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="section-customize">
+                                <div className="customize-border">
+                                    <div className="outer-bg">
+                                        <div className="bg-image section-outstanding-doctor"></div>
+                                    </div>
+                                    <div className="postion text-center">
+                                        <div className="section-customize-text">Phó Giáo sư, Tiến sĩ, Bác sĩ cao cấp Nguyễn Duy Hưng 2</div>
+                                        <div className="">Cơ Xương Khớp</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="section-customize">
+                                <div className="customize-border">
+                                    <div className="outer-bg">
+                                        <div className="bg-image section-outstanding-doctor"></div>
+                                    </div>
+                                    <div className="postion text-center">
+                                        <div className="section-customize-text">Phó Giáo sư, Tiến sĩ, Bác sĩ cao cấp Nguyễn Duy Hưng 3</div>
+                                        <div className="">Cơ Xương Khớp</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="section-customize">
+                                <div className="customize-border">
+                                    <div className="outer-bg">
+                                        <div className="bg-image section-outstanding-doctor"></div>
+                                    </div>
+                                    <div className="postion text-center">
+                                        <div className="section-customize-text">Phó Giáo sư, Tiến sĩ, Bác sĩ cao cấp Nguyễn Duy Hưng 4</div>
+                                        <div className="">Cơ Xương Khớp</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="section-customize">
+                                <div className="customize-border">
+                                    <div className="outer-bg">
+                                        <div className="bg-image section-outstanding-doctor"></div>
+                                    </div>
+                                    <div className="postion text-center">
+                                        <div className="section-customize-text">Phó Giáo sư, Tiến sĩ, Bác sĩ cao cấp Nguyễn Duy Hưng 5</div>
+                                        <div className="">Cơ Xương Khớp</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="section-customize">
+                                <div className="customize-border">
+                                    <div className="outer-bg">
+                                        <div className="bg-image section-outstanding-doctor"></div>
+                                    </div>
+                                    <div className="postion text-center">
+                                        <div className="section-customize-text">Phó Giáo sư, Tiến sĩ, Bác sĩ cao cấp Nguyễn Duy Hưng 6</div>
+                                        <div className="">Cơ Xương Khớp</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </Slider>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        isLoggedIn: state.user.isLoggedIn,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(OutStandingDoctor);
